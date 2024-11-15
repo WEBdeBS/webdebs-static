85c6cbc2a19bcdfdf64153136c71eddedf13c30d
Antonio Molinari <molinari@incode.it>
2024-11-15 16:10:30 +0100

    wip

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+</script>
+
+<template>
+  <main>
+    <img class="logo" src="./assets/logo_webdebs_basic_black.png" alt="logo" />
+  </main>
+</template>
+
+<style scoped>
+.logo {
+  width: 40vw;
+  max-width: 400px;
+}
+</style>
